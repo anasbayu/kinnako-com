f1c53e47d6de4ae98f60438eb8010c155dc34ff9
Mukhamad Mukhsin Wibowo <mmwibowo2@gmail.com>
2016-01-10 23:39:21 +0700

    Fix animation issue for Chrome

--- js/home.js
+++ js/home.js
@@ -1,10 +1,13 @@
 $(window).ready(function() {
-    $("html").animate(
+    $("html, body").animate(
         {
             scrollTop: 0
         },
+        // "slow"
         500
-    )
+    );
+
+    // return false;
 });
 
 $(".menu-item a").click(function(event) {
@@ -12,14 +15,19 @@ $(".menu-item a").click(function(event) {
 
     var address = $(this).attr("href");
 
-    $("html").animate(
+    $("html, body").animate(
         {
             scrollTop: $("div[id=" + address + "]").offset().top - 50
         },
+        // "slow"
         500
     );
 
-    console.log($("div[id=" + address + "]").offset().top - 50);
+    // return false;
+});
+
+$(".menu-item li a").hover(function() {
+    // $(this).css("color", "#fa3434");
 });
 
 $(window).scroll(function() {
@@ -30,30 +38,39 @@ $(window).scroll(function() {
     }
 
     if ($(this).scrollTop() < $("#section-1").offset().top -250) {
-        $(".main-menu li a").css("color", "#383838");
+        // $(".main-menu li a").css("color", "#383838");
+        $(".main-menu li").removeClass("item-selected");
     }
 
     if ($(this).scrollTop() >= $("#section-1").offset().top -250 &&
         $(this).scrollTop() < $("#section-2").offset().top -250) {
-        $(".main-menu li a").css("color", "#383838");
-        $(".main-menu li:nth-child(1) a").css("color", "#fa3434");
+        // $(".main-menu li a").css("color", "#383838");
+        // $(".main-menu li:nth-child(1) a").css("color", "#fa3434");
+        $(".main-menu li:nth-child(1)").addClass("item-selected");
+        $(".main-menu li:nth-child(1)").siblings().removeClass("item-selected");
     }
 
     if ($(this).scrollTop() >= $("#section-2").offset().top -250 &&
         $(this).scrollTop() < $("#section-3").offset().top -250) {
-        $(".main-menu li a").css("color", "#383838");
-        $(".main-menu li:nth-child(2) a").css("color", "#fa3434");
+        // $(".main-menu li a").css("color", "#383838");
+        // $(".main-menu li:nth-child(2) a").css("color", "#fa3434");
+        $(".main-menu li:nth-child(2)").addClass("item-selected");
+        $(".main-menu li:nth-child(2)").siblings().removeClass("item-selected");
     }
 
     if ($(this).scrollTop() >= $("#section-3").offset().top -250 &&
         $(this).scrollTop() < $("#section-4").offset().top -250) {
-        $(".main-menu li a").css("color", "#383838");
-        $(".main-menu li:nth-child(3) a").css("color", "#fa3434");
+        // $(".main-menu li a").css("color", "#383838");
+        // $(".main-menu li:nth-child(3) a").css("color", "#fa3434");
+        $(".main-menu li:nth-child(3)").addClass("item-selected");
+        $(".main-menu li:nth-child(3)").siblings().removeClass("item-selected");
     }
 
     if ($(this).scrollTop() >= $("#section-4").offset().top -250 &&
         $(this).scrollTop() < $("#footer").offset().top -250) {
-        $(".main-menu li a").css("color", "#383838");
-        $(".main-menu li:nth-child(4) a").css("color", "#fa3434");
+        // $(".main-menu li a").css("color", "#383838");
+        // $(".main-menu li:nth-child(4) a").css("color", "#fa3434");
+        $(".main-menu li:nth-child(4)").addClass("item-selected");
+        $(".main-menu li:nth-child(4)").siblings().removeClass("item-selected");
     }
 });
